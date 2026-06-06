05901f6af07eba41999647e7b9c5cdf762b17b6f
savageme6ix <maingithomas803@gmail.com>
2026-06-06 11:24:10 +0300

    scripts n npm

--- server.js
+++ server.js
@@ -1,13 +1,4 @@
-import http from 'node:http';
-const port = 8000;
-
-const server = http.createServer((req,res)=>{
-    // res.setHeader('Content-Type', 'text/html');
-    // res.statusCode = 404;
-    res.writeHead(500, {'Content-Type': 'application/json'})
-    res.end(JSON.stringify({message:'Server error'}))
-})
-
-// server.listen(port,()=>{
-//     console.log(`Server running on port ${port}`);
-// });
+import {format} from 'date-fns';
+import {v4 as uuidv4} from 'uuid'
+console.log(uuidv4());
+console.log(format(new Date(), 'yyyyMMdd\tHH:mm:ss'))
