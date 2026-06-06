079e8b11c1ed9d0c9835e6a412dafe89e246d22b
savageme6ix <maingithomas803@gmail.com>
2026-06-06 10:29:43 +0300

    write head

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ import http from 'node:http';
 const port = 8000;
 
 const server = http.createServer((req,res)=>{
-    res.end('hello world')
+    // res.setHeader('Content-Type', 'text/html');
+    // res.statusCode = 404;
+    res.writeHead(500, {'Content-Type': 'application/json'})
+    res.end(JSON.stringify({message:'Server error'}))
 })
 
 server.listen(port,()=>{
