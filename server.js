e941c50c197b0e9a41c5119c385cdbe9e1c5eb71
savageme6ix <maingithomas803@gmail.com>
2026-06-11 11:20:09 +0300

    getting by category

--- server.js
+++ server.js
@@ -3,7 +3,15 @@ const app = express();
 import { products,people } from './app.js';
 
 app.get('/',(req,res)=>{
-    res.json(products)
+    res.send('<h1>Home page</h1><a href="/api/products">products</a>')
+})
+
+app.get('/api/products', (req,res)=>{
+    const newProduct = products.map(()=>{
+        const {id,name} = product;
+        return {id,name}
+    })
+    res.json(newProduct)
 })
 app.listen(5000, ()=>{
     console.log('Server on 5000')
